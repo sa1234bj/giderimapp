84394924eed369f652f918cbeed48b3932447ceb
sevgilime özel <genczihin2025@gmail.com>
2026-09-07 10:17:28 +0300

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,25 @@
+const CACHE = 'giderim-v2';
+const ASSETS = [
+  './giderim.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match('./giderim.html')))
+  );
+});
